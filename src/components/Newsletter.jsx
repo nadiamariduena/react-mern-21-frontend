958e5bd9c93c9bdfde49a86b66a1f267de750069
nadiamariduena <nadia.mm.mariduena@gmail.com>
2021-11-02 21:22:56 +0100

    debut newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+import { Send } from "@material-ui/icons";
+
+//
+//
+const Container = styled.div``;
+const Title = styled.div``;
+const Description = styled.div``;
+//
+const InputContainer = styled.div``;
+const Input = styled.div``;
+const Button = styled.button``;
 
 const Newsletter = () => {
   return (
-    <div>
-      <h1>newsletetr</h1>
-    </div>
+    <Container>
+      <Title></Title>
+
+      <Description></Description>
+
+      <InputContainer>
+        <Input />
+        <Button>
+          <Send />
+        </Button>
+      </InputContainer>
+    </Container>
   );
 };
 
